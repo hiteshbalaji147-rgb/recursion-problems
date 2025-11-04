a1943a2f5dd9d01556e1e1330c98fe667a2e2e7f
hiteshbalaji147-rgb <hiteshbalaji147@gmail.com>
2025-11-04 22:04:33 +0530

    Add program3.cpp - Commit 4: Add optimized power function using divide and conquer

--- program3.cpp
+++ program3.cpp
@@ -14,6 +14,24 @@ double power(double x, int n) {
     return x * power(x, n - 1);
 }
 
+// Optimized power using divide and conquer
+double powerOptimized(double x, int n) {
+    // Base case
+    if (n == 0)
+        return 1;
+
+    // Handle negative exponents
+    if (n < 0)
+        return 1.0 / powerOptimized(x, -n);
+
+    // Divide and conquer
+    double half = powerOptimized(x, n / 2);
+    if (n % 2 == 0)
+        return half * half;
+    else
+        return x * half * half;
+}
+
 int main() {
     double x;
     int n;
@@ -28,6 +46,7 @@ int main() {
         return 1;
     }
 
-    cout << x << "^" << n << " = " << power(x, n) << endl;
+    cout << "\nBasic Recursion: " << x << "^" << n << " = " << power(x, n) << endl;
+    cout << "Optimized Recursion: " << x << "^" << n << " = " << powerOptimized(x, n) << endl;
     return 0;
 }
