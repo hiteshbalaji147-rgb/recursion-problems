5e38f705efe02c45671e4d284ee01f4d93fb4dda
hiteshbalaji147-rgb <hiteshbalaji147@gmail.com>
2025-11-06 21:36:22 +0530

    Add program5.cpp - Commit 2: Add mirror check function using recursion

--- program5.cpp
+++ program5.cpp
@@ -14,6 +14,22 @@ struct Node {
     }
 };
 
+// Check if two trees are mirror images
+bool isMirror(Node* left, Node* right) {
+    // Both empty
+    if (left == nullptr && right == nullptr)
+        return true;
+    
+    // One empty, one not
+    if (left == nullptr || right == nullptr)
+        return false;
+    
+    // Check data and recursively check subtrees
+    return (left->data == right->data) &&
+           isMirror(left->left, right->right) &&
+           isMirror(left->right, right->left);
+}
+
 int main() {
     cout << "Symmetric Binary Tree Program" << endl;
     return 0;
